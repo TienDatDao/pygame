68fba4fa81b30ec048d30dc3e1c4cbb3c86ee68c
unknown <daotiendat02052021@gmail.com>
2024-09-26 20:05:34 +0700

    just change something

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import pygame
+import color
+
+pygame.init()
 
 WIDTH, HEIGHT = 500, 500
 
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+
 pygame.display.set_caption("Draw circle")
 
-BLACK = (0,0,0)
+FPS = 60
+
+def draw_window():
+    WIN.fill(color.BLUE)
+    pygame.display.update()
 
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        
-        WIN.fill(BLACK)
-        pygame.display.update()
+
+        draw_window()
 
     pygame.quit()
 
