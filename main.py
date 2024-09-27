4431de153da5e2b2148ddf5689b9de20b6118b09
Dao Tien Dat <94846751+TienDatDao@users.noreply.github.com>
2024-09-27 15:37:51 +0700

    draw circle code

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 import pygame
 import color
+import numpy as np
 
 pygame.init()
 
-WIDTH, HEIGHT = 500, 500
-
+WIDTH, HEIGHT = 1000, 1000
+BLOCK = 10
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-
 pygame.display.set_caption("Draw circle")
-
 FPS = 60
+R = int(input())
+
+arr_check = [[0 for _ in range(int(WIDTH/BLOCK))] for _ in range(int(HEIGHT/BLOCK))]
+
+def calculate():
+    CENTER_X = int(WIDTH/BLOCK/2)
+    CENTER_Y = int(HEIGHT/BLOCK/2)
+    for i in range(0, int(WIDTH/BLOCK)):
+        for j in range(0, int(HEIGHT/BLOCK)):
+            x = abs(CENTER_X-i)
+            y = abs(CENTER_Y-j)
+            if x*x+y*y <= R*R:
+                arr_check[i][j] = 1
+
+
+    #arr_check[CENTER_X][CENTER_Y] = 1
 
 def draw_window():
-    WIN.fill(color.BLUE)
-    pygame.display.update()
+    WIN.fill(color.BLACK)
+
+def draw_grid():
+    for i in range(0, int(WIDTH/BLOCK)):
+        for j in range(0, int(HEIGHT/BLOCK)):
+            x = i*BLOCK + 1
+            y = j*BLOCK + 1
+            if arr_check[i][j] == 0:
+                pygame.draw.rect(WIN, color.WHITE, (x, y, BLOCK-2, BLOCK-2))
+            else:
+                pygame.draw.rect(WIN, color.YELLOW, (x, y, BLOCK-2, BLOCK-2))
 
 def main():
+    calculate()
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -25,6 +50,10 @@ def main():
                 run = False
 
         draw_window()
+
+        draw_grid()
+        
+        pygame.display.flip()
 
     pygame.quit()
 
